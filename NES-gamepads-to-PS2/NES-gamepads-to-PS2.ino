652a5963101902851225a2c58e795daf4bccd0b2
Peter A. Ukhov <ukhov79@gmail.com>
2023-11-20 23:49:01 +0300

    add libs

--- NES-gamepads-to-PS2/NES-gamepads-to-PS2.ino
+++ NES-gamepads-to-PS2/NES-gamepads-to-PS2.ino
@@ -37,13 +37,29 @@ pult_cfg pult_2 = {
 
 const int TICK = 2;
 
+
+// The order of shifting for the buttons:
+// 0 - A, 1 - B, 2 - Select, 3 - Start, 4 - Up, 5 - Down, 6 - Left, 7 - Right   
+
+// Codes for 
 static const int keys_pult_1_c[] = {'a', 'b', 'c', 'd', 'e', 's', 'd', 'f'};
 static const int keys_pult_2_c[] = {'k', 'h', 'j', 'u', 'y', 'i', 'n', 'm'};
 //===============================================================================
-//  libraries
+//  libraries:
+//  To Read keys from PS/2 keyboard 
+//  https://github.com/PaulStoffregen/PS2Keyboard
+//  To Emulate PS/2 Keyboard
+//  https://github.com/Harvie/ps2dev
 //===============================================================================
-//#include "FidPS2/FidPS2Keyboard.h"
-//#include "FidPS2/FidPS2Host.h"
+#include <PS2Keyboard.h>
+
+const int DataPin = 8;
+const int IRQpin =  5;
+
+PS2Keyboard readkeyboard;
+
+#include <ps2dev.h>    //Emulate a PS/2 device
+PS2dev keyboard(3,2);  //clock, data
 
 
 
@@ -53,7 +69,6 @@ void init_joystick(pult_cfg& cfg)
   pinMode(cfg.data, INPUT_PULLUP);
   pinMode(cfg.clock, OUTPUT);
   pinMode(cfg.latch, OUTPUT);
-
   digitalWrite(cfg.clock, HIGH);
 }
 
@@ -79,19 +94,40 @@ int get_keys_state_joystick(pult_cfg& cfg)
   return keys_state;
 }
 
+//===============================================================================
+//  Setup
+//===============================================================================
+
 void setup()
 {
   // Keyboard.begin();
   init_joystick(pult_1);
   init_joystick(pult_2);
- // fid_ps2kb_init(1, 2);
+
+  readkeyboard.begin(DataPin, IRQpin, PS2Keymap_US);
+
+  // send the keyboard start up
+  while(keyboard.write(0xAA)!=0);
+  
   Serial.begin(9600);
-
-
 }
+
+//===============================================================================
+//  loop
+//===============================================================================
 
 void loop()
 {
+
+  if (readkeyboard.available()) {
+    char c = readkeyboard.read();
+    Serial.print(c);
+  }
+
+  keyboard.write(0x0D); //send another Tab
+  keyboard.write(0xF0);  //tab break
+  keyboard.write(0x0D);  //...
+
   int pult_1_keys = get_keys_state_joystick(pult_1);
   int pult_2_keys = get_keys_state_joystick(pult_2);
   
@@ -109,7 +145,7 @@ void loop()
     Serial.println(pult_2_keys);
   }
 
- // bool fid_ps2kb_read(uint8_t* b);
+ bool fid_ps2kb_read(uint8_t* b);
 
   /*
   for (int i = 0; i < 8; i++) {
