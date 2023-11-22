11e4c1416b155b98faad23ee39b03d2e36f5a870
Peter A. Ukhov <ukhov79@gmail.com>
2023-11-22 23:09:07 +0300

    change code: prepare to first test

--- NES-gamepads-to-PS2/NES-gamepads-to-PS2.ino
+++ NES-gamepads-to-PS2/NES-gamepads-to-PS2.ino
@@ -6,17 +6,45 @@
     Author......... Peter A. Ukhov
     E-mail......... ukhov79 @ gmail.com
     Started........ 11/19/2023
-    Finished....... --/--/----
+    Finished....... not yet (waiting for all components to assembly)
     Updated........ --/--/----
 
 ================================================================================
    Notes
 ================================================================================
+
+WARNING - NOT TESTED YET!!!
+waiting for all components to assembly
+
 */
 
 //===============================================================================
 //  Constants
 //===============================================================================
+
+// Gamepads pins
+#define PULT1_DATA_PIN 17
+#define PULT1_LATCH_PIN 16
+#define PULT1_CLOCK_PIN 15
+#define PULT2_DATA_PIN 6
+#define PULT2_LATCH_PIN 7
+#define PULT2_CLOCK_PIN 8
+
+// Emulator pins to connect into retro PC
+#define SIZIF_KEYBOARD_DATA_PIN 2
+#define SIZIF_KEYBOARD_CLOCK_PIN 3
+
+// PS2 Keyboard pins to connect into Keyboard
+#define PS2_KEYBOARD_DATA_PIN 4
+#define PS2_KEYBOARD_CLOCK_PIN 5
+
+// Time countdown for delay in polling cycles of gamepads
+#define TICK 2
+
+//===============================================================================
+//  Gamepads cpp
+//===============================================================================
+
 struct pult_cfg {
   int data;
   int latch;
@@ -24,45 +52,16 @@ struct pult_cfg {
 };
 
 pult_cfg pult_1 = {
-  .data = 12,
-  .latch = 10,
-  .clock = 13
+  .data = PULT1_DATA_PIN,
+  .latch = PULT1_LATCH_PIN,
+  .clock = PULT1_CLOCK_PIN
 };
 
 pult_cfg pult_2 = {
-  .data = 4,
-  .latch = 5,
-  .clock = 6
+  .data = PULT2_DATA_PIN,
+  .latch = PULT2_LATCH_PIN,
+  .clock = PULT2_CLOCK_PIN
 };
-
-const int TICK = 2;
-
-
-// The order of shifting for the buttons:
-// 0 - A, 1 - B, 2 - Select, 3 - Start, 4 - Up, 5 - Down, 6 - Left, 7 - Right   
-
-// Codes for 
-static const int keys_pult_1_c[] = {'a', 'b', 'c', 'd', 'e', 's', 'd', 'f'};
-static const int keys_pult_2_c[] = {'k', 'h', 'j', 'u', 'y', 'i', 'n', 'm'};
-//===============================================================================
-//  libraries:
-//  To Read keys from PS/2 keyboard 
-//  https://github.com/PaulStoffregen/PS2Keyboard
-//  To Emulate PS/2 Keyboard
-//  https://github.com/Harvie/ps2dev
-//===============================================================================
-#include <PS2Keyboard.h>
-
-const int DataPin = 8;
-const int IRQpin =  5;
-
-PS2Keyboard readkeyboard;
-
-#include <ps2dev.h>    //Emulate a PS/2 device
-PS2dev keyboard(3,2);  //clock, data
-
-
-
 
 void init_joystick(pult_cfg& cfg)
 {
@@ -94,9 +93,33 @@ int get_keys_state_joystick(pult_cfg& cfg)
   return keys_state;
 }
 
+// The order of shifting for the buttons:
+// 0 - A, 1 - B, 2 - Select, 3 - Start, 4 - Up, 5 - Down, 6 - Left, 7 - Right   
+// Codes for 
+byte keys_pult_1_c[] = {0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19};
+// {'a', 'b', 'c', 'd', 'e', 's', 'd', 'f'};
+byte keys_pult_2_c[] = {0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19};
+// {'k', 'h', 'j', 'u', 'y', 'i', 'n', 'm'};
+// TODO: Read from EEPROM, will be configured, change keycodes
+
+//===============================================================================
+//  libraries:
+//  To Read keys from PS/2 keyboard 
+//  https://github.com/PaulStoffregen/PS2Keyboard
+//  To Emulate PS/2 Keyboard
+//  https://github.com/Harvie/ps2dev
+//===============================================================================
+
+#include <EEPROM.h>
+#include <PS2Keyboard.h>    // Read a PS/2 keyboard
+#include <ps2dev.h>         // Emulate a PS/2 device
+
 //===============================================================================
 //  Setup
 //===============================================================================
+
+PS2Keyboard readkeyboard;
+PS2dev keyboard(SIZIF_KEYBOARD_CLOCK_PIN, SIZIF_KEYBOARD_DATA_PIN);
 
 void setup()
 {
@@ -104,11 +127,13 @@ void setup()
   init_joystick(pult_1);
   init_joystick(pult_2);
 
-  readkeyboard.begin(DataPin, IRQpin, PS2Keymap_US);
+  // init for PS/2 keyboard
+  readkeyboard.begin(PS2_KEYBOARD_DATA_PIN, PS2_KEYBOARD_CLOCK_PIN, PS2Keymap_US);
 
-  // send the keyboard start up
+  // send the keyboard start up to SIZIF XXS
   while(keyboard.write(0xAA)!=0);
   
+  // to test PS/2 Keyboard
   Serial.begin(9600);
 }
 
@@ -119,15 +144,18 @@ void setup()
 void loop()
 {
 
+  // Just read something
   if (readkeyboard.available()) {
     char c = readkeyboard.read();
     Serial.print(c);
   }
 
+  // Just print something
   keyboard.write(0x0D); //send another Tab
   keyboard.write(0xF0);  //tab break
   keyboard.write(0x0D);  //...
 
+  // Test and print Gamepads
   int pult_1_keys = get_keys_state_joystick(pult_1);
   int pult_2_keys = get_keys_state_joystick(pult_2);
   
@@ -136,6 +164,15 @@ void loop()
   else {
     Serial.print("Pult 1 = ");
     Serial.println(pult_1_keys);
+
+    for (int i = 0; i < 8; i++) {
+      if (!(pult_1_keys & (1 << i))) {
+        keyboard.write(keys_pult_1_c[i]);
+      } else {
+        keyboard.write(0xF0);
+        keyboard.write(keys_pult_1_c[i]);
+      }
+    }
   }
 
   if(pult_2_keys == 255)
@@ -143,25 +180,16 @@ void loop()
   else {
     Serial.print("Pult 2 = ");
     Serial.println(pult_2_keys);
-  }
 
- bool fid_ps2kb_read(uint8_t* b);
-
-  /*
-  for (int i = 0; i < 8; i++) {
-    if (!(pult_1_keys & (1 << i))) {
-      Keyboard.press(keys_pult_1_c[i]);
-    } else {
-      Keyboard.release(keys_pult_1_c[i]);
-    }
-
-    if (!(pult_2_keys & (1 << i))) {
-      Keyboard.press(keys_pult_2_c[i]);
-    } else {
-      Keyboard.release(keys_pult_2_c[i]);
+    for (int i = 0; i < 8; i++) {
+      if (!(pult_2_keys & (1 << i))) {
+        keyboard.write(keys_pult_2_c[i]);
+      } else {
+        keyboard.write(0xF0);
+        keyboard.write(keys_pult_2_c[i]);
+      }
     }
   }
-  */
 
   delay(20);
 }
